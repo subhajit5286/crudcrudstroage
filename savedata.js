a8b7f76831363b5ea59c7562e439937146290288
FIRST_NAME LAST_NAME <subhajit.bhattacharyya@gmail.com>
2023-02-15 12:37:47 +0530

    saved User Details from crudcrud

--- savedata.js
+++ savedata.js
@@ -4,9 +4,9 @@ var i=0;
 // Form submit event
 form.addEventListener('submit', saveItem);
 // Delete event
-itemList.addEventListener('click', removeItem);
+//itemList.addEventListener('click', removeItem);
 //edit event
-itemList.addEventListener('click', editItem);
+//itemList.addEventListener('click', editItem);
 
 function saveItem(e){
     e.preventDefault();
@@ -54,7 +54,44 @@ axios.post('https://crudcrud.com/api/0020e60e03ea48d8aa688b62ce3ac17c/addPostreq
 .catch((err)=>{
   console.log(err)
 })
-}// Remove item
+}
+window.addEventListener("DOMContentLoaded",()=>{
+  axios.get('https://crudcrud.com/api/0020e60e03ea48d8aa688b62ce3ac17c/addPostrequest/')
+  .then((response)=>{
+    for(var i=0;i<response.data.length;i++){
+      console.log(response.data[i]._id)
+      var li = document.createElement('li');
+    // Add class
+    li.className = 'list-group-item';
+    li.id=response.data[i]._id;
+    // Add text node with input value
+    li.appendChild(document.createTextNode(response.data[i].name));
+    li.appendChild(document.createTextNode('  '));
+    li.appendChild(document.createTextNode(response.data[i].email));
+    // Create del button element
+    var deleteBtn = document.createElement('button');
+  
+    // Add classes to del button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  
+    // Append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+  
+    // Append button to li
+    li.appendChild(deleteBtn);
+    var editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-warning btn-sm float-right warning';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    li.appendChild(editBtn);
+    // Append li to list
+    itemList.appendChild(li);
+    }
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+})
+// Remove item
 /*function removeItem(e){
     if(e.target.classList.contains('delete')){
       if(confirm('Are You Sure?')){
